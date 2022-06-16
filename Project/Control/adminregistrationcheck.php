43764fa45da00457d1977735a2f3ebc10c91d0b9
Rakin Sad Aftab <20-41991-1@student.aiub.edu>
2022-06-17 05:32:53 +0600

    Update adminregistrationcheck.php

--- Project/Control/adminregistrationcheck.php
+++ Project/Control/adminregistrationcheck.php
@@ -2,16 +2,43 @@
 
 $fnameErr = "";
 $lnameErr = "";
-$uname = "";
-$email = "";
-$nid = "";
-$phone = "";
+$unameErr = "";
+$emailErr = "";
+$nidErr = "";
+$phoneErr = "";
 $filepath = "";
 
 if(isset($_POST["submit"]))
 {
     $fname = $_POST["fname"];
+    $lname = $_POST["lname"];
+    $uname = $_POST["uname"];
+    $email = $_POST["email"];
+    $nid = $_POST["nid"];
     $fname = $_POST["fname"];
+
+    if(empty($fname))
+    {
+        $fnameErr = "Please Enter your First Name";
+    }
+    else if(is_numeric($fname))
+    {
+        $fnameErr = "Name can't be numeric";
+    }
+    else
+    {
+        echo "Your First Name is". $fname;
+    }
+    echo "<br>";
+
+    if (empty($lname)) {
+        $fnameErr = "Please Enter your First Name";
+    } else if (is_numeric($lname)) {
+        $fnameErr = "Name can't be numeric";
+    } else {
+        echo "Your First Name is" . $lname;
+    }
+    echo "<br>";
 }
 
 ?>
