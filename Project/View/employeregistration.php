f7f098e4333008657cf5ec43c334ce04015729ef
Sultanul Arifeen Hamim <101488136+Arifeenhamim@users.noreply.github.com>
2022-06-17 00:21:20 +0600

    Update employeregistration.php

--- Project/View/employeregistration.php
+++ Project/View/employeregistration.php
@@ -1,3 +1,8 @@
+<?php
+include("../control/empregistration.php");
+?>
+
+
 <html lang="en">
 
 <head>
@@ -9,9 +14,13 @@
 
 <body>
     <center>
-        <h1>Welcome to XYZ Bank Limited.</h1>
-        <p><strong>To do further work, please register.</strong></p>
-        <br><br><br><br>
+    <h1><font color="green">XYZ Bank Limited.</font></h1>
+    
+    <form action="" method="POST" enctype="multipart/form-data">
+        
+    <p><strong><font color="green">To do further work, please register</font></strong></p>
+    <br><br><br><br>
+        
 
         <form action="" method="POST" enctype="multipart/form-data">
             <center>
@@ -25,7 +34,7 @@
                
                 <font>First name:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; </font> 
       
-                <input type="text" name="fristname" placeholder="Enter Your First name"><br> <br>
+                <input type="text" name="firstname" placeholder="Enter Your First name">  <?php echo $FirstnameErr; ?> <br> <br>
                 
               
                 </td>
@@ -36,7 +45,7 @@
                
                 <label>Last name:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;</label>
                 
-                <input type="text" name="lastname" placeholder="Enter Your Last name"><br><br>
+                <input type="text" name="lastname" placeholder="Enter Your Last name"> <?php echo $LastnameErr; ?>  <br><br>
                 
                 </td>
             </tr>
@@ -48,7 +57,7 @@
                
                 <font>User name:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; </font> 
       
-                <input type="text" name="username" placeholder="Enter your username"><br> <br>
+                <input type="text" name="username" placeholder="Enter your username"><?php echo $UsernameErr;?>    <br> <br>
                 
               
                 </td>
@@ -57,14 +66,14 @@
             <tr>
                         <td>
                             <label>Password:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; </label>
-                            <input type="password" name="password"placeholder="Enter your password"><br><br>
+                            <input type="password" name="password"placeholder="Enter your password"> <?php echo $passwordErr;?> <br><br>
                         </td>
                     </tr>
              
                     <tr>
                         <td>
                             <label>Conform Password :&nbsp;&nbsp;&nbsp;  </label>
-                            <input type="password" name="conformpassword"placeholder="Enter your password"> <br><br>
+                            <input type="password" name="conformpassword"placeholder="Enter your password"><?php echo $confirmPasswordErr;?> <br><br>
                         </td>
                     </tr>
 
@@ -73,7 +82,7 @@
                
                 <font>Email:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; </font> 
       
-                <input type="email" placeholder="Enter your email"> <br><br>
+                <input type="Email" name="email" placeholder="Enter your email"><?php echo $emailErr;?> <br><br>
                 
               
                 </td>
@@ -85,7 +94,7 @@
                         <td>
                            
                                 <label>NID:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;</label> 
-                                <input type="text" name="nid" placeholder="Enter your valid nid"> <br><br>
+                                <input type="text" name="nid" placeholder="Enter your valid nid"> <?php echo $NIDErr;?> <br><br>
                            
                         </td>
                     </tr>
@@ -94,7 +103,7 @@
                         <td>
                            
                                 <label>Phone: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;</label>
-                                <input type="text" name="phone" placeholder="Enter your phone number"> <br><br>
+                                <input type="text" name="phone" placeholder="Enter your phone number"> <?php echo $PhoneErr;?> <br><br>
                          
                         </td>
                     </tr>
@@ -117,8 +126,9 @@
                         <td>
                            
                                 <label>Gender:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;</label> 
-                                <input type="radio" name="gender">Male
-                                <input type="radio" name="gender">Female <br><br>
+                                <input type="radio" name="gender" vale="male" >Male <!-- <?php echo $genderErr;?>  -->
+                                <input type="radio" name="gender" vale="female">Female &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <?php echo $genderErr;?> <br><br>
                            
                         </td>
                     </tr>
@@ -148,6 +158,19 @@
                          
                         </td>
                     </tr>
+
+
+                    <tr>
+                       <td>
+                       <label> Please choose a file: &nbsp;&nbsp;</label>
+               
+                                <input type="file" name ="myfile"> <br>
+                                <?php echo $fileErr; ?> 
+                            
+                               <br>
+                         </td>
+                        </tr>
+
 
          <center>
 
