bc61c392cd3a6dc7b35aff9549dce2c23d80bf3d
Rakin Sad Aftab <20-41991-1@student.aiub.edu>
2022-07-17 20:25:24 +0600

    Update index.php

--- FT Labtask1/View/index.php
+++ FT Labtask1/View/index.php
@@ -16,28 +16,33 @@ include("../Control/process.php");
     <form action="" method="post" enctype="multipart/form-data">
         <table>
             <tr>
+                <td>First Name :</td>
                 <td>
-                    First Name : <input type="text" name="fname" placeholder=" Enter First Name">
+                    <input type="text" name="fname" placeholder=" Enter First Name">
                     <?php echo $nameErr; ?> <br>
-
                 </td>
             </tr>
+
             <tr>
+                <td>Last Name :</td>
                 <td>
-                    Last Name : <input type="text" name="lname" placeholder="Enter Last Name">
+                    <input type="text" name="lname" placeholder="Enter Last Name">
                     <?php echo $name2Err; ?><br>
                 </td>
             </tr>
+
             <tr>
+                <td>Age :</td>
                 <td>
-                    Age : <input type="text" name="age" placeholder="Enter Your Age">
+                    <input type="text" name="age" placeholder="Enter Your Age">
                     <?php echo $AgeErr; ?><br>
                 </td>
             </tr>
-            <tr>
-                <td>
 
-                    Designation : <input type="radio" name="designation" value="juniorprogrammer">Junior Programmer
+            <tr>
+                <td>Designation :</td>
+                <td>
+                    <input type="radio" name="designation" value="juniorprogrammer">Junior Programmer
                     <input type="radio" name="designation" value="seniorprogrammer">Senior Programmer
                     <input type="radio" name="designation" value="projectlead">Project Lead
                 <td>
@@ -45,54 +50,52 @@ include("../Control/process.php");
                 </td><br><br>
                 </td>
             </tr>
+
             <tr>
+                <td>Preferred Language :</td>
                 <td>
-                    Preferred Language :
                     <input type="checkbox" name="PreferredLanguage1" value="java">JAVA
                     <input type="checkbox" name="PreferredLanguage2" value="php"> PHP
                     <input type="checkbox" name="PreferredLanguage2" value="c++"> C++
                     <?php echo $LanErr; ?><br>
                 </td>
-
-
             </tr>
+
             <tr>
+                <td>Email :</td>
                 <td>
-                    Email : <input type="email" name="email" placeholder="Enter Your Email" autocomplete="on">
+                    <input type="email" name="email" placeholder="Enter Your Email" autocomplete="on">
                 <td>
                     <?php echo $emailErr; ?> </td><br>
-
-
                 </td>
             </tr>
+
             <tr>
+                <td>Password :</td>
                 <td>
-                    Password : <input type="password" name="pass" placeholder="Enter Desired Password">
+                    <input type="password" name="pass" placeholder="Enter Desired Password">
                     <?php echo $passErr; ?><br>
                 </td>
             </tr>
 
-
-
-
             <tr>
+                <td>Please choose a photo</td>
                 <td>
-                    Please choose a photo
-
                     <input type="file" name="myfile"> <br>
                     <?php echo $fileErr; ?>
                     <br>
                 </td>
             </tr>
+
             <tr>
                 <td>
                     <input type="submit" name="Submit">
                     <input type="reset">
-
                 </td>
             </tr>
+
+        </table>
     </form>
-    </table>
 </body>
 
 </html>
