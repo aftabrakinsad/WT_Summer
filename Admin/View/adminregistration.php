7c717a51c1016f7587f274532aff71b7f5f984e6
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-07-22 23:30:48 +0600

    Update adminregistration.php

--- Admin/View/adminregistration.php
+++ Admin/View/adminregistration.php
@@ -16,6 +16,8 @@ if (isset($_SESSION['username'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/CSS" href="../CSS/adminregistration.css">
+    <link rel="stylesheet" type="text/CSS" href="../CSS/adminlogin.css">
     <title>Admin Registration</title>
 </head>
 
