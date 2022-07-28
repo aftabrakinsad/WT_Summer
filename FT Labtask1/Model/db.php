9df9b8aded9a9a7427e2b1b441b6b2a385f1ff4e
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-07-28 22:35:07 +0600

    Done

--- FT Labtask1/Model/db.php
+++ FT Labtask1/Model/db.php
@@ -1,4 +1,5 @@
 <?php
+
 class db
 {
 
@@ -9,20 +10,20 @@ class db
         $password = "";
         $dbname = "labtask";
         $conn =  new mysqli($servername, $username, $password, $dbname);
-        if ($conn->connect_error) {
+        if ($conn->connect_error)
+        {
             echo "error connecting database";
         }
         return $conn;
     }
 
-    function insertUser($fname, $lname, $age, $Designation, $planguage, $email, $pass, $picture, $table, $conn)
+    function insertUser($fname, $lname, $age, $designation, $planguage, $email, $pass, $picture, $table, $conn)
     {
-        $sqlstr = "INSERT INTO employee(fname,lname,age,Designation,planguage,email,password,picture)
-        VALUES ('$fname','$lname','$age','$Designation','$planguage','$email','$pass','$picture') ";
+        $sqlstr = "INSERT INTO employee(fname,lname,age,designation,planguage,email,password,picture)
+        VALUES ('$fname','$lname','$age','$designation','$planguage','$email','$pass','$picture') ";
 
         if ($conn->query($sqlstr))
         {
-            echo "<br>";
             echo "Data saved";
         }
         else
@@ -31,3 +32,5 @@ class db
         }
     }
 }
+
+?>
