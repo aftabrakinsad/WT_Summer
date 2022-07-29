fa68244299b232adf41e4e41f5be6a333beecaf0
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-07-30 02:11:31 +0600

    Update

---
+++ Admin/View/forgetpassword.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/CSS" href="../CSS/forgetpassword.css">
+    <title>Forgot Password</title>
+</head>
+
+<body class="bodyimage">
+    <form action="forgetpassword.php" method="POST">
+        <h2 class="h2">Forgot Password</h2>
+        <p>Enter your NID and Email address</p>
+
+        <input type="text" name="nid" placeholder="Enter your nid number">
+
+        <input type="email" name="email" placeholder="Enter email address">
+
+        <input class="inputbutton" type="submit" name="ckeck" value="Continue">
+    </form>
+</body>
+
+</html>
