22b2cce1eeb651cc761db4cf5bf6b64d604c9980
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-08-06 18:05:35 +0600

    Update

--- Admin/Control/applicantdelete.php
+++ Admin/Control/applicantdelete.php
@@ -9,7 +9,6 @@ if(isset($_GET['deleteid']))
     $mydb = new db();
     $myconn = $mydb->openConn();
     $applicant_serial = $_GET['deleteid'];
-    $sqlstr = "DELETE FROM applicantofadmin WHERE applicant_serial = '$applicant_serial'";
     $result = $mydb ->deleting_new_admin($applicant_serial, "applicantofadmin", $myconn);
     
     if($result == true)
--- Admin/Control/newadmin.php
+++ Admin/Control/newadmin.php
@@ -5,6 +5,7 @@
 if (isset($_GET['addingid']))
 {
     $applicant_serial = $_GET['addingid'];
+    
     $mydb = new db();
     $myconn = $mydb->openConn();
     $result = $mydb ->adding_new_admin($applicant_serial,"applicantofadmin", "applicantofadmin", $myconn);
