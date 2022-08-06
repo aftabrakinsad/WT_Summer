1582f23d2b7b75b30f2efbc8ecdfc54d732b4203
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-08-06 18:55:30 +0600

    Update

--- Admin/Control/newadmin.php
+++ Admin/Control/newadmin.php
@@ -2,6 +2,23 @@
 
 @include("../Model/db.php");
 
+if (isset($_GET['addingid']))
+{
+    $applicant_serial = $_GET['addingid'];
 
+    $mydb = new db();
+    $myconn = $mydb->openConn();
+    $applicant_serial = $_GET['addingid'];
+    $result = $mydb ->adding_new_admin("staticadmin", "applicantofadmin", $myconn);
+
+    if($result == true)
+    {
+        header("location: ../View/adminrequest.php");
+    }
+    else
+    {
+        echo "error";
+    }
+}
 
 ?>
