d98fa8335609274f61a887dc691f7ec44a79219c
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-08-14 20:23:46 +0600

    Mid Term Lab Task
    Simple form and printing values from text box, check box and radio button.

--- Lab Task - 2/Control/process.php
+++ Lab Task - 2/Control/process.php
@@ -1,9 +1,15 @@
 <?php
+
 if (isset($_POST['submit']))
 
 {
     $FirsName = $_REQUEST['FirstName'];
-        if (empty($FirsName)) 
+    $LastName = $_REQUEST['LastName'];
+    $Age = $_REQUEST['Age'];
+    $Email = $_POST['Email'];
+    $Password = $_REQUEST['Password'];
+
+    if (empty($FirsName)) 
     {
         echo 'Please Enter a valid First Name';
     }
@@ -11,95 +17,103 @@ if (isset($_POST['submit']))
     {
         echo 'Your First Name is ' . $FirsName;
     }
+
     echo '<br>';
 
-        $LastName = $_REQUEST['LastName'];
-        if (empty($LastName))
+    if (empty($LastName))
     {
         echo 'Please Enter a valid Last Name';
-    } else
+    }
+    else
     {
         echo 'Your Last Name is ' . $LastName;
     }
 
     echo '<br>';
 
-    $Age = $_REQUEST['Age'];
-
-        if ($Age == '')
+    if ($Age == '')
     {
         echo 'Please Enter your Age ';
-    } else
+    }
+    else
     {
         echo 'Your Age is ' . $Age;
     }
 
     echo '<br>';
 
-    if (
-        isset($_POST['Desgination1']) || isset($_POST['Desgination2']) || isset($_POST['Desgination3']))
+    if (isset($_POST['Desgination1']) || isset($_POST['Desgination2']) || isset($_POST['Desgination3']))
+    {
+        if (isset($_POST['Desgination1']))
         {
-            if (isset($_POST['Desgination1'])) {
-                echo 'Your Have Selected Junior Programmer ';
-                echo '<br>';
-            }
-            if (isset($_POST['Desgination2'])) {
-                echo 'Your Have Selected Senior Programmer';
-                echo '<br>';
-            }
-            if (isset($_POST['Desgination3'])) {
-                echo 'Your Have Selected Project Lead';
-                echo '<br>';
-            }
-    } else {
+            echo 'Your Have Selected Junior Programmer ';
+            echo '<br>';
+        }
+        else if (isset($_POST['Desgination2']))
+        {
+            echo 'Your Have Selected Senior Programmer';
+            echo '<br>';
+        }
+        else if (isset($_POST['Desgination3']))
+        {
+            echo 'Your Have Selected Project Lead';
+            echo '<br>';
+        }
+        else
+        {
+            echo 'Please Select a Designation';
+        }
+    }
+    else
+    {
         echo 'You Have not selected anything';
     }
 
     echo '<br>';
 
-
-    if (
-        isset($_POST['PreferredLanguage1']) ||
-        isset($_POST['PreferredLanguage2']) ||
-        isset($_POST['PreferredLanguage3'])) {
-        if (isset($_POST['PreferredLanguage1'])) {
+    if (isset($_POST['PreferredLanguage1']) || isset($_POST['PreferredLanguage2']) || isset($_POST['PreferredLanguage3']))
+    {
+        if (isset($_POST['PreferredLanguage1']))
+        {
             echo 'Your Have Selected JAVA ';
             echo '<br>';
         }
-
-        if (isset($_POST['PreferredLanguage2'])) {
+        else if (isset($_POST['PreferredLanguage2']))
+        {
             echo 'Your Have Selected PHP';
             echo '<br>';
         }
-        if (isset($_POST['PreferredLanguage3'])) {
+        else if (isset($_POST['PreferredLanguage3']))
+        {
             echo 'Your Have Selected C++';
             echo '<br>';
         }
+        else
+        {
+            echo 'Please Select a Language';
+        }
     } 
-    else {
+    else
+    {
         echo 'You Have not Selected a checkbox';
         echo '<br>';
     }
 
-    $Email = $_POST['Email'];
-    if (
-        empty($Email) ||
-        !preg_match(
-            '/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix',
-            $Email
-        )
-    ) {
+    if (empty($Email) ||!preg_match('/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix', $Email))
+    {
         echo 'Invalid email';
         echo '<br>';
-    } else 
+    }
+    else 
     {
         echo 'Your email is ' . $Email . '<br>';
     }
 
-    $Password = $_REQUEST['Password'];
-    if (strlen($Password) < 8) {
+    if (strlen($Password) < 8)
+    {
         echo 'Enter a valid password';
-    } else
+    }
+    else
     {
         echo 'password is valid';
     }
--- Lab Task - 2/View/index.php
+++ Lab Task - 2/View/index.php
@@ -1,6 +1,9 @@
 <?php
-include("../Control/process.php");
+
+@include("../Control/process.php");
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,78 +18,100 @@ include("../Control/process.php");
     <h1>Registration Form </h1>
     <form action="" method="post">
         <table>
+
             <tr>
                 <td>
                     Fisrt Name :
-                <td>
+                </td>
+
                 <td>
                     <input type="text" name="FirstName">
                 </td>
+            </tr>
+
             <tr>
+
                 <td>
                     Last Name :
-                <td>
+                </td>
+
                 <td>
                     <input type="text" name="LastName">
                 </td>
+            </tr>
+
             <tr>
                 <td>
                     Age :
-                <td>
+                </td>
+
                 <td>
                     <input type="text" name="Age">
                 </td>
             </tr>
+
             <tr>
                 <td>
                     Designation :
-                <td>
+                </td>
+
                 <td>
                     <input type="radio" name="Desgination1">Junior Programmer
                     <input type="radio" name="Desgination2">Senior Programmer
                     <input type="radio" name="Desgination3">Project Lead
                 </td>
             </tr>
+
             <tr>
                 <td>
                     Preferred Language :
-                <td>
+                </td>
+
                 <td>
                     <input type="checkbox" name="PreferredLanguage1">JAVA
                     <input type="checkbox" name="PreferredLanguage2"> PHP
                     <input type="checkbox" name="PreferredLanguage3"> C++
                 </td>
             </tr>
+
             <tr>
                 <td>
                     Email :
-                <td>
+                </td>
+
                 <td>
                     <input type="email" name="Email">
                 </td>
             </tr>
+
             <tr>
                 <td>
                     Password :
-                <td>
+                </td>
+
                 <td>
                     <input type="password" name="Password">
                 </td>
             </tr>
+            
             <tr>
                 <td>
                     Please choose a file
-                <td>
+                </td>
+
                 <td>
                     <input type="file">
                 </td>
             </tr>
+
             <tr>
                 <td>
                     <input type="submit" name="submit">
                     <input type="reset" name="Reset">
                 </td>
             </tr>
+        </table>
+    </form>
 </body>
 
 </html>
