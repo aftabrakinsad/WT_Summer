9558e4791df18f2c862cf5acfd252e9c526e0c12
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-08-15 00:18:40 +0600

    Logic and Validation for Admin Request
    After filling the form with all the correct information and required steps this form will submit.

--- Banking Managment System/Admin/Control/adminregistrationcheck.php
+++ Banking Managment System/Admin/Control/adminregistrationcheck.php
@@ -35,87 +35,90 @@ if(isset($_POST["submit"]))
     {
         $unameErr = "!Username must be more then 5 characters!";
     }
-        if ($email != "" && !preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $email))
+    if ($email != "" && !preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $email))
+    {
+        $emailErr = "Enter your valid email";
+    }
+    if($nid != "" && !is_numeric($nid))
+    {
+        $nidErr = "NID must have numeric value";
+    }
+    if(!isset($_POST["gender"]))
+    {
+        $genderErr = "You have not selected any Gender";
+    }
+    if(isset($_POST["gender"]))
+    {
+        if($_POST["gender"] == "male")
         {
-            $emailErr = "Enter your valid email";
+            
         }
-        if($nid != "" && !is_numeric($nid))
+        if ($_POST["gender"]=='female')
         {
-            $nidErr = "NID must have numeric value";
+          
         }
-        if(!isset($_POST["gender"]))
+        $gender =   $_POST["gender"];
+    }
+    if($password != "")
+    {
+        if (!$uppercase || !$lowercase || !$number || !$specialchars || strlen($password) < 5)
         {
-            $genderErr = "You have not selected any Gender";
+            $passwordErr = "!Password should be at least 5 characters in length and should include at least one upper case, one lower case, one number and one special character!";
         }
-            if(isset($_POST["gender"]))
+    }
+    if ($password == $cpassword &&$fname != "" && $lname != "" &&  $phone != "" && $nid != "")
+    {
+        if ($fname != "" && $lname != "" &&  $phone != "")
+        {
+            echo $_FILES["myfile"]["name"];
+            if (move_uploaded_file($_FILES["myfile"]["tmp_name"], "../Uploads/" . $_FILES["myfile"]["name"]))
             {
-                if($_POST["gender"] == "male")
-                {
-                    
-                }
-                if ($_POST["gender"]=='female')
-                {
-                  
-                }
-            $gender =   $_POST["gender"];
+                $filepath = "../Uploads/" . $_FILES["myfile"]["name"];
+                $fileErr = "File Uploaded";
             }
-        if($password != "")
-        {
-            if (!$uppercase || !$lowercase || !$number || !$specialchars || strlen($password) < 5)
+            else
             {
-                $passwordErr = "!Password should be at least 5 characters in length and should include at least one upper case, one lower case, one number and one special character!";
+                
             }
-        }
-        if ($password == $cpassword &&$fname != "" && $lname != "" &&  $phone != "" && $nid != "")
-        {
-            if ($fname != "" && $lname != "" &&  $phone != "")
+
+            echo "<br>";
+
+            $existingadminregistrationdata = file_get_contents('../Data/adminregistrationdata.json');
+            $datadecode = json_decode($existingadminregistrationdata, true);
+            $adminregistrationdata = array
+            (
+                'firstName' => $fname,
+                'lastName' => $lname,
+                'username' => $uname,
+                'gender' => $gender,
+                'email' => $email,
+                'nid' => $nid,
+                'phone' => $phone,
+                'password' => $password,
+                'cpassword' => $cpassword,
+                'filepath' => $filepath
+            );
+            $datadecode[] = $adminregistrationdata;
+            $dataencode = json_encode($datadecode, JSON_PRETTY_PRINT);
+
+            if (file_put_contents('../Data/adminregistrationdata.json', $dataencode))
             {
-                    echo $_FILES["myfile"]["name"];
-                    if (move_uploaded_file($_FILES["myfile"]["tmp_name"], "../Uploads/" . $_FILES["myfile"]["name"]))
-                    {
-                        $filepath = "../Uploads/" . $_FILES["myfile"]["name"];
-                        $fileErr = "File Uploaded";
-                    }
-                    else
-                    {
-                        echo "";
-                    }
-                    echo "<br>";
-                    $existingadminregistrationdata = file_get_contents('../Data/adminregistrationdata.json');
-                    $datadecode = json_decode($existingadminregistrationdata, true);
-                    $adminregistrationdata = array
-                    (
-                        'firstName' => $fname,
-                        'lastName' => $lname,
-                        'username' => $uname,
-                        'gender' => $gender,
-                        'email' => $email,
-                        'nid' => $nid,
-                        'phone' => $phone,
-                        'password' => $password,
-                        'cpassword' => $cpassword,
-                        'filepath' => $filepath
-                    );
-                        $datadecode[] = $adminregistrationdata;
-                        $dataencode = json_encode($datadecode, JSON_PRETTY_PRINT);
-                        if (file_put_contents('../Data/adminregistrationdata.json', $dataencode))
-                        {
-                            echo "Request is being submitted, Please wait for the responce";
-                        }
-                        else
-                        {
-                            echo "Registration incomplete";
-                        }
+                echo "Request is being submitted, Please wait for the responce";
             }
-                        else
-                        {
-                            echo "Please Fill Out This Form";
-                        }
+            else
+            {
+                echo "Registration incomplete";
+            }
         }
         else
         {
-            $cpasswordErr = "Password didn't match";
+            echo "Please Fill Out This Form";
         }
+    }
+    else
+    {
+        $cpasswordErr = "Password didn't match";
+    }
 }
 if(isset($_POST["login"]))
 {
