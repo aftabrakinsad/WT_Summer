d227736454fc9651397700a45fe73c07a4192161
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-08-14 22:30:03 +0600

    Mid Term Lab Task
    Storing data in JSON file from input box, check box and radio button.

--- Lab-3/Control/process.php
+++ Lab-3/Control/process.php
@@ -1,180 +1,188 @@
 <?php
-$nameErr = "";
-$name2Err = "";
-$AgeErr = "";
-$passErr = "";
-$emailErr = "";
-$fileErr = "";
-$desigErr ="";
-$LanErr ="";
-$designation="";
-$Language="";
-$fromdata ="";
-$filepath ="";
+  $nameErr = "";
+  $name2Err = "";
+  $AgeErr = "";
+  $passErr = "";
+  $emailErr = "";
+  $fileErr = "";
+  $desigErr ="";
+  $LanErr ="";
+  $Designation="";
+  $Language="";
+  $fromdata ="";
+  $filepath ="";
+
 if(isset($_POST["Submit"]))
 {
-
-$name = $_POST["fname"];
-$name2 = $_POST["lname"];
-$Age = $_POST["age"];
-
-$pass = $_POST["pass"];
-$email = $_POST["email"];
-if(isset($Language))
-{
-    $LanErr = 'You Havent Selected a checkbox';
-}
-else{$Language = $_POST["PreferredLanguage"] ;}
-if(isset($designation))
-{
-    $desigErr = 'You have not selected any Designation';
-}
-else{$designation = $_POST["designation"];}
+    $name = $_POST["fname"];
+    $name2 = $_POST["lname"];
+    $Age = $_POST["age"];
+    $pass = $_POST["pass"];
+    $email = $_POST["email"];
+    $Designation = $_POST["designation"];
 
     if (empty($name))
     {
-        $nameErr ='Please Enter First Name';
+        $nameErr = 'Please Enter First Name';
     }
-        else if(is_numeric($name))
-        {
-        $nameErr ='First Name can not be numeric';
-      }
-         else
+    else if (is_numeric($name))
     {
-        echo 'Your First Name is ' . $name;
+        $nameErr = 'First Name can not be numeric';
     }
+    else
+    {
+        echo 'Your First Name formet is valid';
+    }
+
     echo '<br>';
 
-   
-        if (empty ($name2))
+    if (empty($name2))
     {
         $name2Err = 'Please Enter Last Name';
     }
-    else if(is_numeric($name2))
+    else if (is_numeric($name2))
     {
-      $name2Err ='Last Name can not be numeric';
+        $name2Err = 'Last Name can not be numeric';
     }
-       else
+    else
     {
-        echo 'Your Last Name is ' . $name2;
+        echo 'Your Last Name formet is valid';
+    }
+
+    echo '<br>';
+
+    if (empty($Age))
+    {
+        $AgeErr = 'Please Enter your Age ';
+    }
+    else if(!is_numeric($Age))
+    {
+        $AgeErr = "Age can't be a character";
+    }
+    else
+    {
+        echo 'Your Age formet is valid';
     }
 
     echo '<br>';
 
-   
-
-        if (empty ($Age))
+    if (isset($Designation))
     {
-        $AgeErr= 'Please Enter your Age ';
-    } else
-    {
-        echo 'Your Age is ' . $Age;
-    }
-
-    echo '<br>';
-   
-    if (
-        isset($designation) )
+        if ("juniorprogrammer" == $Designation)
         {
-        if ("juniorprogrammer"== $designation) {
-            $desigErr = 'You selected Junior Programmer ';
-         
+            echo "You selected Junior Programmer";
         }
-        if ($designation=='seniorprogrammer') {
-            $desigErr = 'You selected Senior Programmer';
-         
+        else if ($Designation == 'seniorprogrammer')
+        {
+            echo "You selected Senior Programmer";
         }
-        if ($designation=='projectlead') {
-            $desigErr ='You selected Project Lead';
-         
+        else if ($Designation == 'projectlead')
+        {
+            echo "You selected Project Lead";
         }
-    } else {
-        $desigErr = 'You have not selected any Designation';
+        else
+        {
+            $desigErr = 'You have not selected any Designation';
+        }
     }
 
     echo '<br>';
 
-   
-    if (
-        isset($_POST['PreferredLanguage1']) ||
-        isset($_POST['PreferredLanguage2']) ||
-        isset($_POST['PreferredLanguage3'])) {
-        if (isset($_POST['PreferredLanguage1'])) {
-            $LanErr = 'Your Have Selected JAVA ';
-            $Language = $Language. $_POST['PreferredLanguage1'];
-            echo '<br>';
+    if (isset($_POST['PreferredLanguage1']) || isset($_POST['PreferredLanguage2']) || isset($_POST['PreferredLanguage3']))
+    {
+        if (isset($_POST['PreferredLanguage1']))
+        {
+            echo "Your Have Selected JAVA";
+            $Language = $Language . $_POST['PreferredLanguage1'];
         }
-   
-        if (isset($_POST['PreferredLanguage2'])) {
-            $LanErr = 'Your Have Selected PHP';
-            $Language =  $Language.','. $_POST['PreferredLanguage2'];
-            echo '<br>';
+        else if (isset($_POST['PreferredLanguage2']))
+        {
+            echo "Your Have Selected PHP";
+            $Language =  $Language . '' . $_POST['PreferredLanguage2'];
         }
-        if (isset($_POST['PreferredLanguage3'])) {
-            $LanErr = 'Your Have Selected C++';
-            $Language = $Language.','. $_POST['PreferredLanguage2'];
-            echo '<br>';
+        else if (isset($_POST['PreferredLanguage3']))
+        {
+            echo "Your Have Selected C++";
+            $Language = $Language . '' . $_POST['PreferredLanguage2'];
+        }
+        else
+        {
+            $LanErr = 'You Have not Selected a checkbox';
         }
     }
-   
-      else {
-        $LanErr = 'You Have not Selected a checkbox';
-        echo '<br>';
-    }
 
+    echo '<br>';
 
- 
-
-    if(empty($email)  )
+    if (empty($email))
     {
         $emailErr = "You must enter email";
     }
-    else if(!preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix",$email))
-    { $emailErr = "Wrong Pattern Email";}
-    else{
-      echo "Your email is ".$email;
-      echo '<br>';
+    else if (!preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $email))
+    {
+        $emailErr = "Wrong Pattern Email";
+    }
+    else
+    {
+        echo "Your email formet is valid";
     }
 
-   
-    if (strlen($pass) < 5) {
-        $passErr =  'Enter a valid password <br>';
-    } else
-     {
-        echo 'password is valid ';
+    echo '<br>';
+
+    if (strlen($pass) < 5)
+    {
+        $passErr =  'Enter a valid password';
+    }
+    else
+    {
+        echo 'password formet is valid';
     }
 
-echo $_FILES["myfile"]["name"];
+    echo '<br>';
 
-if(move_uploaded_file($_FILES["myfile"]["tmp_name"],"../uploads/".$_FILES["myfile"]["name"]))
-{ $filepath = "../uploads/".$_FILES["myfile"]["name"];
-    $fileErr = "File Uploaded";
+    if(move_uploaded_file($_FILES["myfile"]["tmp_name"],"../uploads/".$_FILES["myfile"]["name"]))
+    { 
+        $filepath = "../uploads/".$_FILES["myfile"]["name"];
+        $formdata = array(
+            'firstname'=>$_POST["fname"],
+            'lastname'=>$_POST["lname"],
+            'age'=>$_POST["age"],
+            'pass'=>$_POST["pass"],
+            'email'=>$_POST["email"],
+            'language'=>$Language,
+            'designation'=>$_POST["designation"],
+            'filepath'=>$filepath
+        );
+        $existingdata = file_get_contents('../data/data.json');
+        $tempJSONdata = json_decode($existingdata);
+        $tempJSONdata[] =$formdata;
+        $jsondata = json_encode($tempJSONdata, JSON_PRETTY_PRINT);
+        
+        if(file_put_contents("../data/data.json",$jsondata))
+        {
+            echo "File Uploaded :- " . $_FILES["myfile"]["name"];
+            echo '<br>';
+            echo 'Data Successfully Saved';
+            echo '<br><br><br>';
+        }
+        else
+        {
+            echo 'No Data Saved';
+            echo '<br><br><br>';
+        }
+
+        echo "Showing saved data from json file(data.json) <br>";
+        $data = file_get_contents("../data/data.json");
+        $mydata = json_decode($data);
+        foreach ($mydata as $myobject)
+        {
+            foreach ($myobject as $key => $value)
+            {
+                echo $key . " => " . $value . "<br>";
+            }
+        }
+    }
+    else
+    {
+        $fileErr = "Upload Error";
+    }
 }
-
-else
-$fileErr = 'Upload Error';
-$formdata = array(
-    'firstname'=>$_POST["fname"],
-    'lastname'=>$_POST["lname"],
-    'age'=>$_POST["age"],
-    'pass'=>$_POST["pass"],
-    'email'=>$_POST["email"],
-    'language'=>$Language,
-    'designation' => $designation,
-    'filepath'=>$filepath
-);
-$existingdata = file_get_contents('../Data/data.json');
-$tempJSONdata = json_decode($existingdata);
-$tempJSONdata[] =$formdata;
-
-
-$jsondata = json_encode($tempJSONdata, JSON_PRETTY_PRINT);
-if(file_put_contents("../Data/data.json",$jsondata))
-{
-    echo 'Data Successfully Saved';
-}
-else
-echo 'No Data Saved';
-
-}
-?>
--- Lab-3/View/index.php
+++ Lab-3/View/index.php
@@ -1,84 +1,124 @@
 <?php
 include("../Control/process.php");
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-    <body>
-   
-        <h1> Registration Form </h1>
-        <form action ="" method ="post" enctype ="multipart/form-data">
-            <table>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form Using JSON</title>
+</head>
+
+<body>
+    <?php
+
+    echo $nameErr;
+    echo $name2Err;
+    echo $AgeErr;
+    echo $desigErr;
+    echo $LanErr;
+    echo $emailErr;
+    echo $passErr;
+    echo $fileErr;
+    
+    ?>
+    <h1> Website Registration Form </h1>
+    <p>@<strong>Rakin Sad Aftab</strong></p>
+    <form action="" method="post" enctype="multipart/form-data">
+
+        <table>
+
             <tr>
                 <td>
-         First Name : <input type= "text" name ="fname" placeholder =" Enter First Name">   <?php echo $nameErr; ?> <br>
-       
-</td>
-</tr>
-<tr>
-    <td>
-         Last Name : <input type ="text" name ="lname" placeholder = "Enter Last Name">  <?php echo $name2Err; ?><br>
-</td>
-</tr>
-<tr>
-    <td>
-    Age :     <input type ="text" name ="age" placeholder = "Enter Your Age"> <?php echo $AgeErr; ?><br>
-</td>
-</tr>
-<tr>
-    <td>
+                    First Name :
+                </td>
+                <td>
+                    <input type="text" name="fname" placeholder=" Enter First Name"> <br>
+                </td>
+            </tr>
 
-         Designation : <input type = "radio" name = "designation" value ="juniorprogrammer">Junior Programmer
-         <input type = "radio" name = "designation" value ="seniorprogrammer">Senior Programmer
-         <input type = "radio" name = "designation" value ="projectlead">Project Lead <td> <?php echo $desigErr; ?></td><br><br>
-</td>
-</tr>
-<tr>
-<td>
-    Preferred Language :          
-    <input type="checkbox" name="PreferredLanguage1"  value="java" >JAVA
-    <input type="checkbox" name="PreferredLanguage2"  value="php"> PHP
-    <input type="checkbox" name="PreferredLanguage2"  value="c++"> C++
-    <?php echo $LanErr; ?><br>
- </td>
+            <tr>
+                <td>
+                    Last Name :
+                </td>
+                <td>
+                    <input type="text" name="lname" placeholder="Enter Last Name"> <br>
+                </td>
+            </tr>
 
+            <tr>
+                <td>
+                    Age :
+                </td>
+                <td>
+                    <input type="text" name="age" placeholder="Enter Your Age"> <br>
+                </td>
+            </tr>
 
-</tr>
-<tr>
-    <td>
-   Email : <input type ="email" name = "email" placeholder = "Enter Your Email" autocomplete ="on"><td> <?php echo $emailErr; ?> </td><br>
- 
+            <tr>
+                <td>
+                    Designation :
+                </td>
+                <td>
+                    <input type="radio" name="designation" value="juniorprogrammer">Junior Programmer
+                    <input type="radio" name="designation" value="seniorprogrammer">Senior Programmer
+                    <input type="radio" name="designation" value="projectlead">Project Lead
+                    <br>
+                </td>
+            </tr>
 
-</td>
-</tr>
-<tr>
-    <td>
-         Password : <input type ="password" name ="pass" placeholder = "Enter Desired Password"><?php echo $passErr; ?><br>
-</td>
-</tr>
+            <tr>
+                <td>
+                    Preferred Language :
+                </td>
+                <td>
+                    <input type="checkbox" name="PreferredLanguage1" value="java">JAVA
+                    <input type="checkbox" name="PreferredLanguage2" value="php"> PHP
+                    <input type="checkbox" name="PreferredLanguage2" value="c++"> C++
+                    <br>
+                </td>
+            </tr>
 
+            <tr>
+                <td>
+                    Email :
+                </td>
+                <td>
+                    <input type="email" name="email" placeholder="Enter Your Email" autocomplete="on"> <br>
+                </td>
+            </tr>
 
+            <tr>
+                <td>
+                    Password :
+                </td>
+                <td>
+                    <input type="password" name="pass" placeholder="Enter Desired Password"> <br>
+                </td>
+            </tr>
 
+            <tr>
+                <td>
+                    Please choose a file
+                </td>
+                <td>
+                    <input type="file" name="myfile"> <br>
+                </td>
+            </tr>
 
-<tr>
-     <td>
-      Please choose a file
+            <tr>
+                <td>
+                    <input type="submit" name="Submit">
+                    <input type="reset">
+                </td>
+            </tr>
 
-      <input type="file" name ="myfile"> <br>
-      <?php echo $fileErr; ?>
-     <br>
-    </td>
-</tr>
-<tr>
-    <td>
-    <input type ="submit" name="Submit" >
-    <input type = "reset">
+        </table>
 
-</td>
-</tr>
+    </form>
+</body>
 
-   
-
-</form>
-    </table>
-   
-    </body>
-    </head>
+</html>
