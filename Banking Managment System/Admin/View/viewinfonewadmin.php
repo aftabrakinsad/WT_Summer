ba1992afa4d38918bee204862f7b54d4ad9b7ef1
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-08-14 23:52:18 +0600

    View Admin Information Page
    Those who applied for the admin post, we can see their information from this page.

--- Banking Managment System/Admin/View/viewinfonewadmin.php
+++ Banking Managment System/Admin/View/viewinfonewadmin.php
@@ -1,27 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>New Admin Information</title>
 </head>
-<body bgcolor="skyblue">
-    <tr>
-        <td>
-            <a href="../View/adminhomepage.php">GO Back</a>
-        </td>
-    </tr>
-    <center>
-        <tr>
-            <td>
-                <h1>Admins Registration Informations</h1>
-            </td>
-        </tr>
 
-        <tr>
-            <?php include("../Control/viewnewadminregistrations.php"); ?>
-        </tr>
+<body bgcolor="skyblue">
+    <a href="../View/adminhomepage.php">GO Back</a>
+
+    <center>
+        <h1>Admins Registration Informations</h1>
+
+        <table>
+            <tr>
+                <td>
+                    <?php @include("../Control/viewnewadminregistrations.php"); ?>
+                </td>
+            </tr>
+        </table>
     </center>
+
 </body>
+
 </html>
