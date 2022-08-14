4c272ca4eb088d42e66c37c8b453008275033c18
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-08-15 00:41:19 +0600

    Employee Home Page
    After logging employee will be redirected to this page.

--- Banking Managment System/Employee/view/epage1.php
+++ Banking Managment System/Employee/view/epage1.php
@@ -1,6 +1,8 @@
 <?php
+
 session_start();
-if(empty($_SESSION["username"]) && empty($_SESSION["password"]))
+
+if (empty($_SESSION["username"]) && empty($_SESSION["password"]))
 {
     header("location: ../view/employelogin.php");
 }
@@ -9,34 +11,33 @@ if(empty($_SESSION["username"]) && empty($_SESSION["password"]))
 
 <!DOCTYPE html>
 <html lang="en">
-<center>
-<header>
 
-            <h1><font color="green">XYZ Bank Limited.</font></h1>
-            <p><strong><font color="green">Employe Homepage</font></strong></p>
-            <br><br><br><br>
-        </header>
-
-        
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Employe Page </title>
+    <title>Employee - Home </title>
 
 </head>
 
 <body bgcolor="skyblue">
+    <center>
 
-<tr>
-                        <td>
-                      <h3> <a href="viewinformation.php">View Employe Information </a></h3>  <br>
-                        </td>
-                    </tr>
-                    <tr>
-    <h2> <strong> <font color="blue"> Welcome to Your Home Page<?php include('../control/cookie.php'); ?> </font></strong> </h2> <br><br>
-   
-    <h3>Do you want to <a href="../control/employelogout.php">Logout</a></h3>
+        <header>
+            <h1><font color="green">City Bank Limited.</font></h1>
+            <p><strong><font color="green">Employe Homepage</font></strong></p>
+            <br><br><br><br>
+        </header>
+
+        <h3> <a href="viewinformation.php">View Employe Information </a></h3>
+        <br>
+
+        <h2> <strong><font color="blue"> Welcome to Your Home Page<?php include('../control/cookie.php'); ?> </font></strong> </h2> 
+        <br><br>
+
+        <h3>Do you want to <a href="../control/employelogout.php">Logout</a></h3>
+
+    </center>
 </body>
-</center>
+
 </html>
