583bfd6d8ae34da5df40214cf0e7365ca7891a0d
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-08-15 00:20:32 +0600

    Cookie Process
    We are showing your username as a cookie

--- Banking Managment System/Admin/Control/cookie.php
+++ Banking Managment System/Admin/Control/cookie.php
@@ -1,11 +1,14 @@
 <?php
+
 	$cookie_name="username";
+
 	if(!isset($_COOKIE["username"]))
 	{
 		echo " ";
 	}
 	else
 	{
-		echo ", ".$_COOKIE[$cookie_name];
+		echo ", " .$_COOKIE[$cookie_name];
 	}
+	
 ?>
