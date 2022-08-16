92fb5b67eaf48096bdb7b1e6f5d5b6db5678505a
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-08-17 00:31:20 +0600

    Update forgetpassword.php

--- Admin/View/forgetpassword.php
+++ Admin/View/forgetpassword.php
@@ -28,11 +28,13 @@
         <h2 class="h2">Forgot Password</h2>
 
         <?php
-        if (count($errors) > 0) {
+        if (count($errors) > 0)
+        {
         ?>
             <div>
                 <?php
-                foreach ($errors as $showerror) {
+                foreach ($errors as $showerror)
+                {
                     echo "<p id='denger'>$showerror</p>";
                 }
                 ?>
