46bdc3f277176932892fdc0fe7852e9d4bc1bc0f
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-08-17 22:21:22 +0600

    Update

--- Admin/Control/adminmanage.process.php
+++ Admin/Control/adminmanage.process.php
@@ -62,29 +62,4 @@ if(isset($_POST["submit"]))
     }
 }
 
-$mydb = new db();
-$myconn = $mydb->openConn();
-$resultXD = $mydb->retrive_from_details_table_for_selected_admins("details_table_for_selected_admins", $myconn);
-
-if ($resultXD == true) {
-    foreach ($resultXD as $row) 
-    {
-        echo '<tr>
-            <td>' . $row['fname'] . '</td>
-            <td>' . $row['lname'] . '</td>
-            <td>' . $row['uname'] . '</td>
-            <td>' . $row['email'] . '</td>
-            <td>' . $row['nid'] . '</td>
-            <td>' . $row['phone'] . '</td>
-            <td>' . $row['salary'] . '</td>
-            <td>' . $row['accountno'] . '</td>
-        </tr>';
-    }
-}
-else 
-{
-
-}
-
-
 ?>
--- Admin/View/adminmanage.php
+++ Admin/View/adminmanage.php
@@ -43,71 +43,53 @@ if (empty($_SESSION["username"]) && empty($_SESSION["password"])) {
             <div>
                 <button type="submit" name="search">Search</button>
             </div>
-            </div>
+        </div>
 
-            <div class="flex-container">
-                <div class=container>
-                    <label>First Name :</label><br>
-                    <input name="fname" type="text" value="<?php echo $fname; ?>">
-                </div>
-                <div class=container>
-                    <label>Last Name :</b></label><br>
-                    <input name="lname" type="text" value="<?php echo $lname; ?>">
-                </div>
-                <div class=container>
-                    <label>Username :</label><br>
-                    <input name="uname" type="text" value="<?php echo $uname; ?>" readonly>
-                </div>
+        <div class="flex-container">
+            <div class=container>
+                <label>First Name :</label><br>
+                <input name="fname" type="text" value="<?php echo $fname; ?>">
             </div>
-            <div class="flex-container">
-                <div class=container>
-                    <label>Email-ID :</b></label><br>
-                    <input name="phone" type="text" value="<?php echo $email; ?>">
-                </div>
-                <div class=container>
-                    <label>NID :</label><br>
-                    <input name="nid" type="text" value="<?php echo $nid; ?>" readonly>
-                </div>
-                <div class=container>
-                    <label>Phone No. :</b></label><br>
-                    <input name="phone" type="text" value="<?php echo $phone; ?>">
-                </div>
+            <div class=container>
+                <label>Last Name :</b></label><br>
+                <input name="lname" type="text" value="<?php echo $lname; ?>">
             </div>
-            <div class="flex-container">
-                <div class=container>
-                    <label>Salary :</b></label><br>
-                    <input name="salary" type="text" value="<?php echo $salary; ?>">
-                </div>
-                <div class=container>
-                    <label>Account No. :</b></label><br>
-                    <input name="accountno" type="text" value="<?php echo $accountno; ?>">
-                </div>
-                <div>
-                    <input class="button" type="submit" name="submit" id="submit" value="update">
-                </div>
+            <div class=container>
+                <label>Username :</label><br>
+                <input name="uname" type="text" value="<?php echo $uname; ?>" readonly>
             </div>
+        </div>
+        <div class="flex-container">
+            <div class=container>
+                <label>Email-ID :</b></label><br>
+                <input name="phone" type="text" value="<?php echo $email; ?>">
+            </div>
+            <div class=container>
+                <label>NID :</label><br>
+                <input name="nid" type="text" value="<?php echo $nid; ?>" readonly>
+            </div>
+            <div class=container>
+                <label>Phone No. :</b></label><br>
+                <input name="phone" type="text" value="<?php echo $phone; ?>">
+            </div>
+        </div>
+        <div class="flex-container">
+            <div class=container>
+                <label>Salary :</b></label><br>
+                <input name="salary" type="text" value="<?php echo $salary; ?>">
+            </div>
+            <div class=container>
+                <label>Account No. :</b></label><br>
+                <input name="accountno" type="text" value="<?php echo $accountno; ?>" readonly>
+            </div>
+            <div>
+                <input class="button" type="submit" name="submit" id="submit" value="update">
+                <a class="button" href="../View/selected_admin.php">Show Admins</a>
+            </div>
+        </div>
+
     </form>
 
-    <form action="" method="POST">
-        <table>
-            <caption>Information Of Selceted Admin</ caption>
-            <thead>
-                <tr>
-                    <th scope="col">First Name</th>
-                    <th scope="col">Last Name</th>
-                    <th scope="col">User Name</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">NID</th>
-                    <th scope="col">Phone</th>
-                    <th scope="col">salary</th>
-                    <th scope="col">accountno</th>
-                </tr>
-            </thead>
-            <tbody>
-                
-            </tbody>
-        </table>
-    </form>
 </body>
 
 </html>
