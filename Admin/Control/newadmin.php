b5094b5268a551e34d448478701a81ebae3958d6
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-08-23 17:53:10 +0600

    Control - New Admin

--- Admin/Control/newadmin.php
+++ Admin/Control/newadmin.php
@@ -17,8 +17,11 @@ if(isset($_GET['email']))
     else
     {
         $result = $mydb -> adding_new_admin($email, "staticadmin", "applicantofadmin", $myconn);
+
         $resultII = $mydb -> adding_new_adminII($email, "details_table_for_selected_admins", "applicantofadmin", $myconn);
+
         $resultIII = $mydb -> adding_new_admin_accountno_info($email, "admin_account_number", "applicantofadmin", $myconn);
+        
         $resultIV = $mydb -> deleting_after_selecting($email,"applicantofadmin", $myconn);
 
         if($result == true && $resultII == true && $resultIII == true && $resultIV == true)
