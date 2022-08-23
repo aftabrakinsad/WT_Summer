8528faf7e7c1e43c3daf1ea44ee495f1f31fbf47
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-08-23 17:43:48 +0600

    Control - Email Check via AJAX

--- Admin/Control/ajax.emailcheck.php
+++ Admin/Control/ajax.emailcheck.php
@@ -8,14 +8,14 @@ if(!empty($email))
 {
     $mydb = new db();
     $myconn = $mydb -> openConn();
-    $results = $mydb -> searching_existing_email($email,"staticadmin", $myconn);
+    $results = $mydb -> searching_existing_email($email, "staticadmin", $myconn);
     if($results -> num_rows > 0)
     {
         echo "Email Exsists";
     }
     else
     {
-        echo "Email doesn't Exists";
+
     }
 }
 
