21301d7e702b123e14b21e7d623a4065b74b75ee
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-08-23 15:19:26 +0600

    View - News
    Structure of this page.

--- Admin/View/News.php
+++ Admin/View/News.php
@@ -12,7 +12,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/news.css">
+    <link rel="stylesheet" type="text/CSS" href="../CSS/news.css">
     <title>News From Admin</title>
 </head>
 
@@ -20,7 +20,9 @@
     <form action="">
         <p id="customer">
             <?php
+            
             @include("../Control/viewnews.process.php");
+            
             ?>
         </p>
         <a id="btn" href="http://localhost/Bankin%20Managment%20System/Admin/View/Home.php">Back To Home</a>
