94380bfe97e76e3622402e44f7679c9ed0ba7aa0
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-08-23 17:19:48 +0600

    Control - Added User
    Retrieving added user information.

--- Admin/Control/addedused.process.php
+++ Admin/Control/addedused.process.php
@@ -12,20 +12,20 @@ if($result == true)
     {
         $user_id = $row['user_id'];
         $email = $row['email'];
-        echo '<tr>
-        <td>'.$row['user_id'].'</td>
-        <td>'.$row['fname'].'</td>
-        <td>'.$row['lname'].'</td>
-        <td>'.$row['uname'].'</td>
-        <td>'.$row['email'].'</td>
-        <td>'.$row['nid'].'</td>
-        <td>'.$row['phone']. '</td>
-        <td>' . $row['accountno'] . '</td>
-        <td>' . $row['pin'] . '</td>
-
-        <td>
-            <a class="reject" href="../Control/userdelete.php?deleteid='. $user_id.'">Delete User</a>
-        </td>
+        echo '
+        <tr>
+            <td>' . $row['user_id']   .'</td>
+            <td>' . $row['fname']     .'</td>
+            <td>' . $row['lname']     .'</td>
+            <td>' . $row['uname']     .'</td>
+            <td>' . $row['email']     .'</td>
+            <td>' . $row['nid']       .'</td>
+            <td>' . $row['phone']     . '</td>
+            <td>' . $row['accountno'] . '</td>
+            <td>' . $row['pin']       . '</td>
+            <td>
+                <a class="reject" href="../Control/userdelete.php?deleteid='. $user_id .'">Delete User</a>
+            </td>
         </tr>';
     }
 }
