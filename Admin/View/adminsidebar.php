16d81f1af1e1f8346ad941c88e2736aed5262f8f
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-08-23 15:06:11 +0600

    View - Admin Side Bar
    Structure of the side bar.

--- Admin/View/adminsidebar.php
+++ Admin/View/adminsidebar.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/adminsidebar.css">
+    <link rel="stylesheet" type="text/CSS" href="../CSS/adminsidebar.css">
 </head>
 
 <body>
