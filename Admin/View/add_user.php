a701fccaac6d707c91deb73043a9bfcee612d631
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-08-23 14:50:37 +0600

    View - Add User
    Structure of this page.

--- Admin/View/add_user.php
+++ Admin/View/add_user.php
@@ -1,6 +1,9 @@
 <?php
+
 session_start();
-if (empty($_SESSION["username"]) && empty($_SESSION["password"])) {
+
+if (empty($_SESSION["username"]) && empty($_SESSION["password"]))
+{
     header("location: ../View/adminlogin.php");
 }
 
