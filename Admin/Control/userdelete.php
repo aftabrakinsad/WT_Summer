58bcafaa049368139525538f6ac70ef5063fb4be
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-08-23 18:14:34 +0600

    Control - User Delete

--- Admin/Control/userdelete.php
+++ Admin/Control/userdelete.php
@@ -9,7 +9,7 @@ if(isset($_GET['deleteid']))
     $mydb = new db();
     $myconn = $mydb->openConn();
     $user_id = $_GET['deleteid'];
-    $result = $mydb-> deleting_added_user($user_id, "new_user",  $myconn);
+    $result = $mydb-> deleting_added_user($user_id, "new_user", $myconn);
     
     if($result == true)
     {
@@ -17,7 +17,7 @@ if(isset($_GET['deleteid']))
     }
     else
     {
-        echo "error";
+        echo "Can't find user to delete";
     }
 }
 
