4cbbc1134bf551ed24e8ba22e4fa848bf0cbdc44
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-08-23 17:52:05 +0600

    Control - Manage Employee

--- Admin/Control/manageemp.process.php
+++ Admin/Control/manageemp.process.php
@@ -12,19 +12,23 @@ if($result == true)
     {
         $id = $row['id'];
         $email = $row['email'];
-        echo '<tr>
-        <td>'.$row['id'].'</td>
-        <td>'.$row['fname'].'</td>
-        <td>'.$row['lname'].'</td>
-        <td>'.$row['uname'].'</td>
-        <td>'.$row['email'].'</td>
-        <td>'.$row['nid'].'</td>
-        <td>'.$row['phone'].'</td>
+        
+        echo '
+        <tr>
+            <td>' . $row['id']    . '</td>
+            <td>' . $row['fname'] . '</td>
+            <td>' . $row['lname'] . '</td>
+            <td>' . $row['uname'] . '</td>
+            <td>' . $row['email'] . '</td>
+            <td>' . $row['nid']   . '</td>
+            <td>' . $row['phone'] . '</td>
 
-        <td><img src="../uploads/'.$row['picture']. '" width="100px" height="100px"></td>
-        <td>
-            <a class="reject" href="../Control/empdelete.php?deleteid='.$id.'">Delete Employee</a>
-        </td>
+            <td>
+                <img src="../uploads/'.$row['picture']. '" width="100px" height="100px">
+            </td>
+            <td>
+                <a class="reject" href="../Control/empdelete.php?deleteid='.$id.'">Delete Employee</a>
+            </td>
         </tr>';
     }
 }
