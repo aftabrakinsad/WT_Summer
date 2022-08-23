888035916a1e9feff2dc588d7a74afa0090f3c3e
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-08-23 15:24:07 +0600

    View - Selected Admins
    Structure of this page. Showing the selected admins informations.

--- Admin/View/selected_admin.php
+++ Admin/View/selected_admin.php
@@ -21,7 +21,7 @@ if (empty($_SESSION["username"]) && empty($_SESSION["password"]))
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Selected Admins</title>
-    <link rel="stylesheet" href="../CSS/adminrequest.css">
+    <link rel="stylesheet" type="text/CSS" href="../CSS/adminrequest.css">
 </head>
 
 <body>
