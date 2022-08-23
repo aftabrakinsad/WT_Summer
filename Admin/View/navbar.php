2379d39f391289bd1ea53fdf59b5616446d35086
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-08-23 15:18:19 +0600

    View - Navbar
    Structure of the navigation bar.

--- Admin/View/navbar.php
+++ Admin/View/navbar.php
@@ -3,16 +3,12 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/navbar.css">
-    <script src="jquery-3.2.1.min.js"></script>
+    <link rel="stylesheet" type="text/CSS" href="../CSS/navbar.css">
 </head>
 
 <body>
     <div class="nav-wrapper">
         <div class="topnav" id="theTopNav">
-            <a href="javascript:void(0);" class="icon" onclick="openNav()" id="hamburger">
-                &#9776;
-            </a>
             <a id="user">Welcome Admin<?php include('../Control/cookie.php'); ?></a>
             <a id="logout" href="../Control/adminlogout.php">Logout</a>
         </div>
