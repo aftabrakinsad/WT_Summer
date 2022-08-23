664f3ed1f3785e5e60d621aea48fe6309d28d1ec
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-08-23 15:04:59 +0600

    View - Admin Side Bar
    Structure of the side bar.

--- Admin/View/adminsidebar.php
+++ Admin/View/adminsidebar.php
@@ -4,12 +4,10 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CSS/adminsidebar.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
 
 <body>
     <div class="sidenav" id="theSideNav">
-        <a href="javascript:void(0)" id="closebtn" onclick="closeNav()">&times;</a>
         <a href="../View/adminhomepage.php">Home</a>
         <a href="../View/atm.php">ATM</a>
         <a id="label">Admin Workspace</a>
