28c330f84f05442979b06383beb8d971d4141f0c
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-08-23 18:09:27 +0600

    Control - User

--- Admin/Control/user.process.php
+++ Admin/Control/user.process.php
@@ -12,22 +12,23 @@ if($result == true)
     {
         $user_id = $row['user_id'];
         $email = $row['email'];
-        echo '<tr>
-        <td>'.$row['user_id'].'</td>
-        <td>'.$row['fname'].'</td>
-        <td>'.$row['lname'].'</td>
-        <td>'.$row['uname'].'</td>
-        <td>'.$row['email'].'</td>
-        <td>'.$row['nid'].'</td>
-        <td>'.$row['phone'].'</td>
-        <td>'.$row['accountno'].'</td>
-
-
-        <td><img src="../uploads/'.$row['picture']. '" width="100px" height="100px"></td>
-        <td>
-            <a class="accept" href="../View/updateuser.php">Update</a> <br><br><br>
-            <a class="reject" href="../Control/applicantdelete.php?deleteid='. $user_id.'">Remove</a>
-        </td>
+        echo '
+        <tr>
+            <td>'.$row['user_id'].'</td>
+            <td>'.$row['fname'].'</td>
+            <td>'.$row['lname'].'</td>
+            <td>'.$row['uname'].'</td>
+            <td>'.$row['email'].'</td>
+            <td>'.$row['nid'].'</td>
+            <td>'.$row['phone'].'</td>
+            <td>'.$row['accountno'].'</td>
+            <td>
+                <img src="../uploads/'.$row['picture']. '" width="100px" height="100px">
+            </td>
+            <td>
+                <a class="accept" href="../View/updateuser.php">Update</a> <br><br><br>
+                <a class="reject" href="../Control/applicantdelete.php?deleteid='. $user_id .'">Remove</a>
+            </td>
         </tr>';
     }
 }
