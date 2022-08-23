2e53118f3319ffe0131f67ee30d7ec76e2c99025
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-08-23 14:53:03 +0600

    View - Add Salary
    Structure of this page.

--- Admin/View/addsalary.php
+++ Admin/View/addsalary.php
@@ -1,7 +1,9 @@
 <?php
 
 session_start();
-if (empty($_SESSION["username"]) && empty($_SESSION["password"])) {
+
+if (empty($_SESSION["username"]) && empty($_SESSION["password"]))
+{
     header("location: ../View/adminlogin.php");
 }
 
@@ -42,7 +44,8 @@ if (empty($_SESSION["username"]) && empty($_SESSION["password"])) {
                     ?>
                 </div>
             <?php
-            } else if (count($success) > 0) 
+            }
+            else if (count($success) > 0) 
             {
             ?>
                 <div>
