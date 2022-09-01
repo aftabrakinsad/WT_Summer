99eb0828252d40a79b82ab6c628b4ae39d9c7ace
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-09-01 20:15:51 +0600

    View - User Interface

---
+++ JQUERY/View/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic Jquery</title>
+</head>
+
+<header>
+    Welcome to the Jquey Session.<br>
+    <sub>By <abbr>Rakin Sad Aftab</abbr></sub>
+</header>
+
+<body>
+    <h2>Click here to view my profile</h2>
+
+    
+</body>
+
+</html>
