2c0de5abc12a858e0b6e43cd8370b256ce286926
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-09-01 20:31:10 +0600

    View - User Interface

--- JQUERY/View/index.php
+++ JQUERY/View/index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <title>Basic Jquery</title>
 </head>
 
@@ -14,9 +15,11 @@
 </header>
 
 <body>
-    <h2>Click here to view my profile</h2>
+    <h2 id="click">Click here to view my profile</h2>
 
-    
+    <p>Hello Man</p>
+
+    <script src="../JS/jquery.js"></script>
 </body>
 
 </html>
