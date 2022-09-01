95f10c3f2652204e04c1a54aa9aa8947540e20d1
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-09-01 21:12:14 +0600

    JS - Jquery

--- JQUERY/JS/jquery.js
+++ JQUERY/JS/jquery.js
@@ -2,9 +2,22 @@ function click()
 {
     $(document).ready(function()
     {
+        //After Clicking, the Div animation
+        $("#click").click(function()
+        {
+            $("p").animate({left: '500px'}, "slow");
+        });
+
+        //After Clicking, Show and Hide the Div
         $("#click").click(function()
         {
             $("p").toggle();
+        });
+
+        //After 1000 mili second, the Div will be fade out
+        $("#click").click(function()
+        {
+            $("p").delay(1000).fadeOut();
         });
     });
 }
